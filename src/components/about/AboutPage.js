ee4b5f1dee47c87a51e9a48bfc37c8a784f8390b
Rizwan Ahmed <rizwan@zekiexperts.com>
2020-04-29 16:06:26 +0500

    Completed a funtioning Router

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AboutPage = () => (
+	<div>
+		<h2>About Page</h2>
+		<p>This App uses React, Redux, React Router and many other libraries.</p>
+	</div>
+);
+
+export default AboutPage;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import HomePage from './components/home/HomePage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './components/App';
+import './index.css';
 
-function Hi() {
-  return <p>Hi.</p>;
-}
-
-render(<Hi />, document.getElementById("app"));
+render(
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById('app')
+);
